268ae4480391f0bdd7af8a5511d4733fc9ebf331
Anmol Nagpal <anmolnagpal@users.noreply.github.com>
2026-03-21 08:52:47 -0400

    fix: consolidate versions.tf, remove provider_meta, upgrade to azurerm >= 4.0

--- versions.tf
+++ versions.tf
@@ -1,22 +1,14 @@
-##-----------------------------------------------------------------------------
-## Versions
-##-----------------------------------------------------------------------------
-# Terraform version
 terraform {
   required_version = ">= 1.10.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.116.0"
+      version = ">= 4.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">=3.6.0"
+      version = ">= 3.6"
     }
-  }
-
-  provider_meta "azurerm" {
-    module_name = "terraform-az-modules/terraform-azurerm-sentinel"
   }
 }
